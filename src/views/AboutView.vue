221468f177fb0fecc628d509cd836bea880cc2fc
张译帆 <zhangyifn@yonyou.com>
2023-05-21 20:55:54 +0800

    reproduciton repo

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script setup lang="ts">
+let arr:[string,number] = ['1',2]
+console.log(arr)
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
